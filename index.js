35897feca17a72c5391611f67c813954d64fcb0f
Jonathan Martinez <jonathanmtzcon@gmail.com>
2024-09-02 15:32:16 -0600

    Added about me section

--- index.js
+++ index.js
@@ -21,19 +21,39 @@ window.addEventListener("resize", () => {
 const linksArray = Array.prototype.slice.call(document.getElementsByTagName("a"))
 linksArray.forEach(link => {
     link.addEventListener("click", event => {
+        if(link.classList.contains("icon-link")){
+            return
+        }
         event.preventDefault()
-        let sectionId = link.textContent
+        const sectionId = link.textContent
             .toLowerCase()
             .replace(" ","-")
             .concat("-section")
         if(sectionId === "-section"){
-            sectionId = "main-section"
+            window.scroll({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }else{
+            const sectionEl = document.getElementById(sectionId)
+            const y = sectionEl.getBoundingClientRect().top + window.scrollY;
+            window.scroll({
+                top: y,
+                behavior: 'smooth'
+            });
         }
-        const sectionEl = document.getElementById(sectionId)
-        const y = sectionEl.getBoundingClientRect().top + window.scrollY;
-        window.scroll({
-            top: y,
-            behavior: 'smooth'
-        });
+        if(window.innerWidth < 600){
+            menuEl.style.display = "none" 
+        }
     })
 })
+
+document.querySelector('.html5').style.setProperty('--c','#E5532E');
+document.querySelector('.css3').style.setProperty('--c','#2D53E5');
+document.querySelector('.javascript').style.setProperty('--c','#F7E029');
+document.querySelector('.react').style.setProperty('--c','#17D9FF');
+document.querySelector('.firebase').style.setProperty('--c','#FFCD36');
+document.querySelector('.flutter').style.setProperty('--c','#65CBF8');
+document.querySelector('.dart').style.setProperty('--c','#0E5D9E');
+document.querySelector('.api').style.setProperty('--c','purple');
+document.querySelector('.git').style.setProperty('--c','#F1553B');
