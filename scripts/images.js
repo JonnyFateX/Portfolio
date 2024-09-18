ab703d2906243c688cdad28964383bcf30f84fb0
Jonathan Martinez <jonathanmtzcon@gmail.com>
2024-09-18 11:38:56 -0600

    Refactored header

---
+++ scripts/images.js
@@ -0,0 +1,30 @@
+const pfpEl = document.getElementById("pfp")
+let index = 0
+const imgSources = [
+    "./assets/killua_pfp.jpg",
+    "./assets/me_pfp.jpg",
+]
+
+setTimeout(() => {
+    spinImg()
+    setInterval(() => spinImg(), 20000)
+}, 6000)
+
+function getImgSrc(){
+    if(index >= imgSources.length){
+        index = 0
+    }
+    return imgSources[index]
+}
+
+function spinImg(){
+    if(pfpEl.classList[0] === "spin"){
+        pfpEl.classList = "pfp"
+    }else{
+        pfpEl.classList = "spin pfp"
+    }
+    setTimeout(() => {
+        index++
+        pfpEl.src = getImgSrc()
+    }, 1200)
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,24 @@
-const menuEl = document.getElementById("menu-list")
 const menuButtonEl = document.getElementById("menu-button")
+const menuListEl = document.getElementById("menu-list")
 
 menuButtonEl.addEventListener("click", () => {
-    if(!(menuEl.style.display) || menuEl.style.display === "none"){
-        menuEl.style.display = "flex"
+    if(menuListEl.style.display === "none" || !(menuListEl.style.display)){
+        menuListEl.style.display = "flex"
+        setTimeout(() => {
+            menuListEl.style.opacity = '1'
+        }, 1)
+        
     }else{
-        menuEl.style.display = "none"
+        menuListEl.style.opacity = '0'
+        setTimeout(() => {
+            menuListEl.style.display = "none"
+        }, 250)
     }
 })
 
-window.addEventListener("resize", () => {
-    const width = window.innerWidth
-    if(width > 700){
-        menuEl.style.display = "flex"
-    }else{
-        menuEl.style.display = "none"
-    }
+menuListEl.addEventListener("click", () => {
+    menuListEl.style.opacity = '0'
+    setTimeout(() => {
+        menuListEl.style.display = "none"
+    }, 250)
 })
-
-const linksArray = Array.prototype.slice.call(document.getElementsByTagName("a"))
-linksArray.forEach(link => {
-    link.addEventListener("click", event => {
-        if(link.classList.contains("icon-link") || link.classList.contains("article-link")){
-            return
-        }
-        event.preventDefault()
-        const sectionId = link.textContent
-            .toLowerCase()
-            .replace(" ","-")
-            .concat("-section")
-        if(sectionId === "-section"){
-            window.scroll({
-                top: 0,
-                behavior: 'smooth'
-            });
-        }else{
-            const sectionEl = document.getElementById(sectionId)
-            const y = sectionEl.getBoundingClientRect().top + window.scrollY;
-            window.scroll({
-                top: y,
-                behavior: 'smooth'
-            });
-        }
-        if(window.innerWidth < 600){
-            menuEl.style.display = "none" 
-        }
-    })
-})
-
-
-
-document.querySelector('.html5').style.setProperty('--c','#E5532E');
-document.querySelector('.css3').style.setProperty('--c','#2D53E5');
-document.querySelector('.javascript').style.setProperty('--c','#F7E029');
-document.querySelector('.react').style.setProperty('--c','#17D9FF');
-document.querySelector('.firebase').style.setProperty('--c','#FFCD36');
-document.querySelector('.flutter').style.setProperty('--c','#65CBF8');
-document.querySelector('.dart').style.setProperty('--c','#0E5D9E');
-document.querySelector('.api').style.setProperty('--c','purple');
-document.querySelector('.git').style.setProperty('--c','#F1553B');
